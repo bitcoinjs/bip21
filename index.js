993b680331b53ddd9d59d7864993bc904bd9653c
Daniel Cousens <github@dcousens.com>
2016-10-27 20:21:04 +1100

    move to exception-less, satoshi value API

--- index.js
+++ index.js
@@ -6,41 +6,40 @@ var qs = require('qs')
 
 function decode (uri) {
   var qregex = /bitcoin:\/?\/?([^?]+)(\?([^]+))?/.exec(uri)
-  if (!qregex) throw new Error('Invalid BIP21 URI: ' + uri)
-
-  var address = qregex[1]
+  if (!qregex) return
 
   // throws if invalid
-  bs58check.decode(address)
+  var address = qregex[1]
+  if (!bs58check.decodeRaw(address)) return
 
   var query = qregex[3]
-  var parsed = qs.parse(query)
+  var options = qs.parse(query)
+  if (options.amount !== undefined) {
+    var amount = parseFloat(options.amount)
+    if (typeof amount !== 'number') return
+    if (!isFinite(amount)) return
+    if (amount < 0) return
 
-  parsed.address = address
-
-  if (parsed.amount) {
-    parsed.amount = parseFloat(parsed.amount)
-
-    if (!isFinite(parsed.amount)) throw new Error('Invalid amount')
-    if (parsed.amount < 0) throw new Error('Invalid amount')
+    // BTC -> satoshis
+    options.amount = amount * 1e8
   }
 
-  return parsed
+  return {
+    address: address,
+    options: options
+  }
 }
 
 function encode (address, options) {
-  // throws if invalid
-  bs58check.decode(address)
+  var _options = {}
+  for (var k in options) _options[k] = options[k]
 
-  options = options || {}
-
-  if (options.amount) {
-    if (!isFinite(options.amount)) throw new TypeError('Invalid amount')
-    if (options.amount < 0) throw new TypeError('Invalid amount')
+  if (_options.amount !== undefined) {
+    // satoshis -> BTC
+    _options.amount /= 1e8
   }
 
-  var query = qs.stringify(options)
-
+  var query = qs.stringify(_options)
   return 'bitcoin:' + address + (query ? '?' : '') + query
 }
 
--- test/index.js
+++ test/index.js
@@ -3,44 +3,28 @@ var fixtures = require('./fixtures')
 var tape = require('tape')
 
 fixtures.valid.forEach(function (f) {
-  if (f.compliant === false) return
+  if (f.compliant !== false) {
+    tape('encodes ' + f.uri, function (t) {
+      var result = bip21.encode(f.address, f.options)
 
-  tape('encodes ' + f.uri, function (t) {
-    var result = bip21.encode(f.address, f.options)
-
-    t.plan(1)
-    t.equal(result, f.uri)
-  })
+      t.plan(1)
+      t.equal(result, f.uri)
+    })
+  }
 
   tape('decodes ' + f.uri + (f.compliant === false ? ' (non-compliant)' : ''), function (t) {
     var decode = bip21.decode(f.uri)
 
-    t.plan(f.options ? 4 : 1)
+    t.plan(2)
     t.equal(decode.address, f.address)
-
-    if (!f.options) return
-    t.equal(decode.amount, f.options.amount !== undefined ? parseFloat(f.options.amount) : undefined)
-    t.equal(decode.label, f.options.label)
-    t.equal(decode.message, f.options.message)
+    t.same(decode.options, f.options)
   })
 })
 
 fixtures.invalid.forEach(function (f) {
-  if (f.address) {
-    tape('throws ' + f.exception + ' for ' + f.uri, function (t) {
-      t.plan(1)
-      t.throws(function () {
-        bip21.encode(f.address, f.options)
-      }, new RegExp(f.exception))
-    })
-  }
+  tape('decode returns nothing for ' + f.uri + ' (' + f.exception + ')', function (t) {
+    if (bip21.decode(f.uri)) t.fail()
 
-  if (f.uri) {
-    tape('throws ' + f.exception + ' for ' + f.uri, function (t) {
-      t.plan(1)
-      t.throws(function () {
-        bip21.decode(f.uri)
-      }, new RegExp(f.exception))
-    })
-  }
+    t.end()
+  })
 })
